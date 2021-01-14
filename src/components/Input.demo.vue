52a1a4e565661b350754aa366645157de2cefbca
不止狂澜 <lshf1437@163.com>
2021-01-14 15:25:04 +0800

    update Input deme

--- src/components/Input.demo.vue
+++ src/components/Input.demo.vue
@@ -3,11 +3,17 @@
 </demo>
 <template>
   <div class="demo">
-    <halo-input v-model:value="value1"/>
+    <halo-input v-model:value="value1" />
     <div>value1: {{ value1 }}</div>
     <halo-input v-model:value="value2" inputType="password" showPassword lable="密码框" />
     <div>value2: {{ value2 }}</div>
-    <halo-input v-model:value="value3" class="demo_input" type="custom" disabled lable="禁用框" />
+    <halo-input
+      v-model:value="value3"
+      class="demo_input"
+      type="custom"
+      disabled
+      lable="禁用框"
+    />
     <div>value3: {{ value3 }}</div>
   </div>
 </template>
@@ -16,13 +22,13 @@
 import { defineComponent, ref } from "vue";
 export default defineComponent({
   setup() {
-    const value1 = ref()
-    const value2 = ref()
-    const value3 = ref(123)
+    const value1 = ref(123);
+    const value2 = ref(123);
+    const value3 = ref(123);
     return {
       value1,
       value2,
-      value3
+      value3,
     };
   },
 });
